aed7b2d5600c92baf52e729db0f9cdb015142e6d
Steve Eriksson <steve.eriksson@gmail.com>
2011-01-05 14:16:28 +0100

    minor fixes

--- Src/Graph/NodeVisitor.hpp
+++ Src/Graph/NodeVisitor.hpp
@@ -12,18 +12,17 @@ class Camera;
  */ 
 class NodeVisitor {
 public:
-  // default implementation does nothing
-  virtual void VisitTransform( Transform* ) {};
-  virtual void VisitGeometry ( Geometry* )  {};
-  virtual void VisitLight    ( Light* )     {};
-  virtual void VisitGroup    ( Group* )     {};
-  virtual void VisitCamera   ( Camera* )    {};
+   // default implementation does nothing
+   virtual void VisitTransform( Transform* ) {};
+   virtual void VisitGeometry ( Geometry* )  {};
+   virtual void VisitLight    ( Light* )     {};
+   virtual void VisitGroup    ( Group* )     {};
+   virtual void VisitCamera   ( Camera* )    {};
+
+   virtual void Traverse( CompositeNode* c ) {};
 
 protected:
-  /** Only subclasses can construct a NodeVisitor */
-  NodeVisitor() {}; // abstract
-
-private:
-  virtual void Traverse( CompositeNode* c );
+   /** Only subclasses can construct a NodeVisitor */
+   NodeVisitor() {}; // abstract
 
 };
