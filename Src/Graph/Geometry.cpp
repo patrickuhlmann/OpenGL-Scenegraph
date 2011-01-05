c88f7fb2e6c8f96bc656fde76133129bd1e1210d
Steve Eriksson <steve.eriksson@gmail.com>
2011-01-05 14:14:45 +0100

    fixed constness

--- Src/Graph/Geometry.cpp
+++ Src/Graph/Geometry.cpp
@@ -7,6 +7,6 @@ void Geometry::Accept( NodeVisitor* visitor ) { visitor->VisitGeometry( this ); 
 
 void Geometry::SetMesh( Mesh* mesh ) { _mesh = mesh; }
 
-Mesh* Geometry::GetMesh() const { return _mesh; };
+const Mesh* Geometry::GetMesh() const { return _mesh; };
 
 void Geometry::SetState( const State& s ) { _state = s; }
