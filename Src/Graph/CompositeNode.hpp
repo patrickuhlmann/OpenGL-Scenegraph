f572c208465060c791dc829ef95d8255ff2712af
Patrick Uhlmann <patrick@eeepaddy.(none)>
2011-01-06 10:42:02 +0100

    Node: added name CompositeNode: added method to Get a node by name (which includes searching all children)

--- Src/Graph/CompositeNode.hpp
+++ Src/Graph/CompositeNode.hpp
@@ -8,6 +8,19 @@ typedef NodeList::iterator NodeIterator;
 
 class CompositeNode : public Node {
 public:
+  virtual Node* GetByName(string Name) {
+    if (this->_name == Name)
+       return this;
+
+    NodeIterator it;
+    for (it=_children.begin(); it != _children.end(); it++) {
+		if (*it._name == Name)
+		  return *it;
+	}
+
+    return 0;
+  }
+
   virtual void Add( Node* n )    {
 	n->SetParent(this);
 	_children.push_back( n ); 
